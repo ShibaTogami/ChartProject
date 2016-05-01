2655328b92f9751ba704b6a20c4196da39658b75
pablo <pablo@DESKTOP-BAUIIMG>
2016-05-01 16:33:22 +0200

    ProyectoServlet añadido. Cambios en ProyectoFacade para realizar una NamedQuery.

--- ChartProject-ejb/src/java/cp/ejb/ProyectoFacade.java
+++ ChartProject-ejb/src/java/cp/ejb/ProyectoFacade.java
@@ -6,9 +6,12 @@
 package cp.ejb;
 
 import cp.entity.Proyecto;
+import java.math.BigDecimal;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,5 +31,15 @@ public class ProyectoFacade extends AbstractFacade<Proyecto> {
     public ProyectoFacade() {
         super(Proyecto.class);
     }
+    
+     public Proyecto findByIdProyecto (BigDecimal idProyecto) {
+        Query q;
+        
+       //q = em.createQuery(
+       //      "select c from Customer c where c.state= :state and c.zip.zipCode=:cp");
+       q = em.createNamedQuery("Proyecto.findByIdProyecto");
+       q.setParameter("idProyecto", idProyecto);
+       return (Proyecto)q.getSingleResult();
+    }  
     
 }
