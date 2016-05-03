1bfbef81e658a8a3ee0bdad098941e931f81bc91
shiba <shiba@LAPTOP-QUKEJHC9>
2016-05-03 18:12:34 +0200

    subida de cosas que no estaban en el git

---
+++ ChartProject-war/src/java/cp/servlet/AuxiliarServlet.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cp.servlet;
+
+import cp.ejb.UsuarioFacade;
+import cp.entity.Usuario;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author pablo
+ */
+@WebServlet(name = "auxiliarServlet", urlPatterns = {"/auxiliarServlet"})
+public class AuxiliarServlet extends HttpServlet {
+
+    @EJB
+    private UsuarioFacade usuarioFacade;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if(request.getParameter("boton") != null) {
+            if(request.getParameter("boton").equals("Anadir")) {
+                request.setAttribute("nombreProyecto", request.getParameter("nombreProyecto"));
+                request.setAttribute("fechaInicioProyecto", request.getParameter("fechaInicioProyecto"));
+                request.setAttribute("descripcionProyecto", request.getParameter("descripcionProyecto"));
+                List<Usuario> participantes = new ArrayList<Usuario>();
+                if (request.getAttribute("participantes") != null) {
+                    participantes = (List<Usuario>) request.getAttribute("participantes");
+                }
+                //Sacamos el usuario de la BD y lo insertamos (si existe) en la lista de participantes
+                if (request.getParameter("nuevoParticipanteProyecto") != null) {
+                    Usuario usuario = (Usuario) usuarioFacade.getUsuarioPorNickname(request.getParameter("nuevoParticipanteProyecto"));
+                    if(usuario != null) {
+                        participantes.add(usuario);
+                    }
+                }
+                RequestDispatcher rd;
+                rd = getServletContext().getRequestDispatcher("/nuevoProyecto.jsp");
+                rd.forward(request, response);
+            } else if (request.getParameter("boton").equals("Guardar")) {
+                request.setAttribute("nombreProyecto", request.getParameter("nombreProyecto"));
+                request.setAttribute("fechaInicioProyecto", request.getParameter("fechaInicioProyecto"));
+                request.setAttribute("descripcionProyecto", request.getParameter("descripcionProyecto"));
+                
+                RequestDispatcher rd;
+                rd = getServletContext().getRequestDispatcher("/nuevoProyectoServlet");
+                rd.forward(request, response);
+            } else {
+                //Error, botón nulo.
+                RequestDispatcher rd;
+                rd = getServletContext().getRequestDispatcher("/nuevoProyecto.jsp");
+                rd.forward(request, response);
+            }
+        } 
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ ChartProject-war/src/java/cp/servlet/NuevoProyectoServlet.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cp.servlet;
+
+import cp.ejb.ProyectoFacade;
+import cp.entity.Proyecto;
+import cp.entity.Usuario;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Date;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author pablo
+ */
+@WebServlet(name = "nuevoProyectoServlet", urlPatterns = {"/nuevoProyectoServlet"})
+public class NuevoProyectoServlet extends HttpServlet {
+
+    @EJB
+    private ProyectoFacade proyectoFacade;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        
+        if(request.getAttribute("nombreProyecto") != null && session.getAttribute("usuario") != null) {
+            long idaux = proyectoFacade.findMaxId().longValue();
+            idaux++;
+            BigDecimal id = BigDecimal.valueOf(idaux);
+           
+            Proyecto proyecto = new Proyecto(id);
+            
+            if(request.getAttribute("fechaInicioProyecto") != null) {
+                proyecto.setFechaInicio((Date) request.getAttribute("fechaInicioProyecto"));
+            }
+            
+            if(request.getAttribute("participantes") != null) {
+                proyecto.setUsuarioCollection((Collection<Usuario>) request.getAttribute("participantes"));
+            }
+            
+            if(request.getAttribute("descripcionProyecto") != null) {
+                proyecto.setDescripcion((String) request.getAttribute("descripcionProyecto"));
+            }
+            
+            this.proyectoFacade.create(proyecto);
+            
+            RequestDispatcher rd;
+            
+            rd = getServletContext().getRequestDispatcher("/proyectoServlet");
+            rd.forward(request, response);
+        } else {
+            //Redirigir al auxiliar servlet
+            RequestDispatcher rd;
+            
+            rd = getServletContext().getRequestDispatcher("/auxiliarServlet");
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
